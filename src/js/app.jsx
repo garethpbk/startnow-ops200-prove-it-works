df7a4402be912c5e99654f3e5416719f0d467275
Gareth Bk <garethbk@gmail.com>
2018-03-12 17:04:33 -0700

    Added mortgage calculator tests

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,10 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='App'>
-        It works!
+      <div className="App">
+        <h1>Mortgage Calculator</h1>
+        <input name="principal" />
+        <input name="interestRate" />
+        <input name="loanTerm" />
+        <select name="period">
+          <option value="12">Monthly</option>
+          <option value="4">Quarterly</option>
+        </select>
+        <button id="calculate">Calculate</button>
+        <p id="output" />
       </div>
     );
   }
---
+++ src/js/lib/Calculator.js
@@ -0,0 +1,21 @@
+/* module.exports = function Calculator() {
+  this.add = function(x, y) {
+    return x + y;
+  };
+};
+ */
+
+module.exports = class Calculator {
+  add(x, y) {
+    return x + y;
+  }
+  subtract(x, y) {
+    return x - y;
+  }
+  multiply(x, y) {
+    return x * y;
+  }
+  divide(x, y) {
+    return x / y;
+  }
+};
